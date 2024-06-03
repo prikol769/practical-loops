8616b450c3a78b5598cf6603c274ae75b8ded168
prikol769 <prikol769@gmail.com>
2024-06-03 16:31:15 -0500

    added Fizz Buzz.

--- app.js
+++ app.js
@@ -9,3 +9,15 @@
 // If a number is not divisible by either 3 or 5, log the number.
 // Remember to commit your solution once it is working.
 
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Fizz Buzz.");
+  }
+  if (i % 3 == 0) {
+    console.log("Fizz.");
+  } else if (i % 5 == 0) {
+    console.log("Buzz.");
+  } else {
+    console.log(i);
+  }
+}
