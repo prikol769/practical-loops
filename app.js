ce7960bc548e9f87420efeb26837de0c623a60d3
prikol769 <prikol769@gmail.com>
2024-06-03 16:31:46 -0500

    fix if with else if

--- app.js
+++ app.js
@@ -12,8 +12,7 @@
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     console.log("Fizz Buzz.");
-  }
-  if (i % 3 == 0) {
+  } else if (i % 3 == 0) {
     console.log("Fizz.");
   } else if (i % 5 == 0) {
     console.log("Buzz.");
